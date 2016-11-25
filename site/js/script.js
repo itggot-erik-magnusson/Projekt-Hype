25acfc3ed4beae66d53eedc8a10d847f971fcbdf
itggot-erik-magnusson <erik.magnusson@itggot.se>
2016-11-25 13:34:27 +0100

    v2
    Vi har lagt till ett "more" fält på förstasidan för att göra den mer
    hype. Denna innehåller koncis info från "about". Vi använde javasript
    för att få knappen att fungera!

---
+++ site/js/script.js
@@ -0,0 +1,6 @@
+function expand() {
+  document.getElementById('top').style.height = '520pt';
+  document.getElementById('top').style.transition = 'ease 2s';
+  document.getElementById('expandBtn').innerHTML = 'keyboard_arrow_up';
+  document.getElementById('topTitle').innerHTML = 'Less';
+}
