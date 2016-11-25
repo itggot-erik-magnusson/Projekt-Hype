0dde7e3ebf58a059e04ce6a07dee1f5bc6d1519b
itggot-erik-magnusson <erik.magnusson@itggot.se>
2016-11-25 14:15:24 +0100

    v3
    Hela första sidan är klar då vi även har implementerat "less" funktion
    till vår "more" sak.

--- site/js/script.js
+++ site/js/script.js
@@ -1,6 +1,15 @@
 function expand() {
-  document.getElementById('top').style.height = '520pt';
+  document.getElementById('top').style.height = '530pt';
   document.getElementById('top').style.transition = 'ease 2s';
-  document.getElementById('expandBtn').innerHTML = 'keyboard_arrow_up';
+  document.getElementById('expandDown').style.display = 'none';
+  document.getElementById('expandUp').style.display = 'block';
   document.getElementById('topTitle').innerHTML = 'Less';
+}
+
+function retract() {
+  document.getElementById('top').style.height = '300pt';
+  document.getElementById('top').style.transition = 'ease 1.5s';
+  document.getElementById('expandDown').style.display = 'block';
+  document.getElementById('expandUp').style.display = 'none';
+  document.getElementById('topTitle').innerHTML = 'More';
 }
