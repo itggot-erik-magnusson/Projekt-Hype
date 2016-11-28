a35acc4771f63fcd32d08f89737fb8756b2b0a72
itggot-erik-magnusson <erik.magnusson@itggot.se>
2016-11-28 14:14:10 +0100

    HUGE IMPROVEMENT!!!!!!!
    Hela about-sidan klar!

--- site/js/script.js
+++ site/js/script.js
@@ -1,6 +1,6 @@
 function expand() {
   document.getElementById('top').style.height = '530pt';
-  document.getElementById('top').style.transition = 'ease 2s';
+  document.getElementById('top').style.transition = 'ease 1.5s';
   document.getElementById('expandDown').style.display = 'none';
   document.getElementById('expandUp').style.display = 'block';
   document.getElementById('topTitle').innerHTML = 'Less';
@@ -8,8 +8,18 @@ function expand() {
 
 function retract() {
   document.getElementById('top').style.height = '300pt';
-  document.getElementById('top').style.transition = 'ease 1.5s';
+  document.getElementById('top').style.transition = 'ease 1s';
   document.getElementById('expandDown').style.display = 'block';
   document.getElementById('expandUp').style.display = 'none';
   document.getElementById('topTitle').innerHTML = 'More';
+}
+
+function menuOn() {
+  document.getElementById('menu').style.height = '230pt';
+  document.getElementById('menu').style.transition = '0.3s';
+}
+
+function menuOff() {
+  document.getElementById('menu').style.height = '0';
+  document.getElementById('menu').style.transition = '0.15s';
 }
